0c0f7051b989a68d228cda3e1ada19f988365fc2
Guillermo Gonzalez <guillermo.gonzalezt@correo.ucu.edu.uy>
2025-10-30 12:11:58 -0300

    ttt

--- UT6/TicTacToe/app/index.tsx
+++ UT6/TicTacToe/app/index.tsx
@@ -16,18 +16,59 @@ export default function Index() {
     null, null, null, null, null, null, null, null, null
   ]);
   
-  const botonPresionado = (position: number) => {
-    const index = position - 1;
-    
-    if (tablero[index] !== null) return;
-    
-    setTablero(tablero => 
-      tablero.map((cell, i) => i === index ? turno : cell)
-    );
-    
-    cambiarTurno();
-    };
 
+  const [visible, setVisible] = React.useState(false)
+
+
+  const [ganador,setGanador] = React.useState("")
+  const checkWin = (board: (string | null)[]): "X" | "O" | null => {
+  const ganadores = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (const [a, b, c] of ganadores) {
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a] as "X" | "O";
+    }
+  }
+  return null;
+};
+
+
+    React.useEffect(() => {
+  const posibleGanador = checkWin(tablero);
+  if (posibleGanador) {
+    setGanador(posibleGanador)
+    setVisible(true)
+  }
+}, [tablero]);
+
+
+
+  const botonPresionado = (position: number) => {
+  const index = position - 1;
+  if (ganador !== "") return;
+  if (tablero[index] !== null) return;
+
+  setTablero(prev => prev.map((cell, i) => (i === index ? turno : cell)));
+  cambiarTurno();
+};
+
+
+
+    const reset = () => {
+      setTablero([null,null,null,null,null,null,null,null,null])
+      setTurno("X")
+      setGanador("")
+      setVisible(false)
+    }
 
 
   return (
@@ -35,33 +76,35 @@ export default function Index() {
       <Text style={styles.titulo}>TicTacToe</Text>
       <View style={styles.grid}>
         <View style={styles.tile}>
-            <TouchableOpacity style={styles.button}><Text style={styles.tiletext}>X</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => botonPresionado(1)}><Text style={styles.tiletext}>{tablero[0]}</Text></TouchableOpacity>
         </View>
         <View style={styles.tile}>
-            <TouchableOpacity style={styles.button}><Text style={styles.tiletext}>O</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => botonPresionado(2)}><Text style={styles.tiletext}>{tablero[1]}</Text></TouchableOpacity>
         </View>
         <View style={styles.tile}>
-            <TouchableOpacity style={styles.button}><Text style={styles.tiletext}></Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => botonPresionado(3)}><Text style={styles.tiletext}>{tablero[2]}</Text></TouchableOpacity>
         </View>
         <View style={styles.tile}>
-            <TouchableOpacity style={styles.button}><Text style={styles.tiletext}></Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => botonPresionado(4)}><Text style={styles.tiletext}>{tablero[3]}</Text></TouchableOpacity>
         </View>
         <View style={styles.tile}>
-            <TouchableOpacity style={styles.button}><Text style={styles.tiletext}></Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => botonPresionado(5)}><Text style={styles.tiletext}>{tablero[4]}</Text></TouchableOpacity>
         </View>
         <View style={styles.tile}>
-            <TouchableOpacity style={styles.button}><Text style={styles.tiletext}></Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => botonPresionado(6)}><Text style={styles.tiletext}>{tablero[5]}</Text></TouchableOpacity>
         </View>
         <View style={styles.tile}>
-            <TouchableOpacity style={styles.button}><Text style={styles.tiletext}></Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => botonPresionado(7)}><Text style={styles.tiletext}>{tablero[6]}</Text></TouchableOpacity>
         </View>
         <View style={styles.tile}>
-            <TouchableOpacity style={styles.button}><Text style={styles.tiletext}></Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => botonPresionado(8)}><Text style={styles.tiletext}>{tablero[7]}</Text></TouchableOpacity>
         </View>
         <View style={styles.tile}>
-            <TouchableOpacity style={styles.button}><Text style={styles.tiletext}></Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => botonPresionado(9)}><Text style={styles.tiletext}>{tablero[8]}</Text></TouchableOpacity>
         </View>
       </View>
+      <Text style={[styles.textoGanador, { opacity: visible ? 1 : 0 }]}>Ganador: {ganador}</Text>
+      <TouchableOpacity onPress={reset} style={styles.reset}><Text style={styles.textoReset}>Reset</Text></TouchableOpacity>
     </View>
   );
 }
@@ -71,7 +114,6 @@ const styles = StyleSheet.create({
   maincontainer:{
     alignItems:"center",
     justifyContent:"center",
-
   },
   titulo:{
     color:"black",
@@ -99,13 +141,34 @@ const styles = StyleSheet.create({
     color:"#ff1717ff",
     fontWeight:"bold",
     fontSize:50,
-
   },
   button:{
     flex:1,
     justifyContent:"center",
     alignItems:"center",
+    
   },
+  reset:{
+    width:200,
+    height:100,
+    backgroundColor:"red",
+    
+    borderRadius:25,
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  textoReset:{
+    color:"white",
+    fontWeight:"bold",
+    fontSize:35,
+  },
+  textoGanador:{
+    marginTop:50,
+    marginBottom:50,
+    fontSize:35,
+    color:"black",
+    opacity:0,
+  }
 
 
 })
